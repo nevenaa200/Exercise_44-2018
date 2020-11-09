c008941c3107c8bd5b70e752f9297edf846e455e
Nevena <44-2018@ftn.edu.rs>
2020-11-09 21:24:25 +0100

    Kreirana klasa ExerciseResult

---
+++ Exercise_44-2018/Exercise_44-2018/ExerciseResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_44_2018
+{
+    public class ExerciseResult
+    {
+
+        private static int idd = 0;
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Point { get; set; }
+
+        public ExerciseResult(string name, string index, int point)
+        {
+            Id = idd++;
+            StudentName = name;
+            StudentIndex = index;
+            Point = point;
+        }
+        public ExerciseResult() { }
+        public override string ToString()
+       => Id + " " + StudentName + " " + StudentIndex + " " + Point;
+    }
+}
+
